3e9ad84d784e3964439d3f39dcdcee74415884d1
waqask0 <muhammadwaqasgaggoo@gmail.com>
2024-09-04 02:04:36 +0500

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    emali:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    }
+},  { timeseries: true });
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
